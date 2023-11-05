527c6b459de9769ea6dc1e1de20989c3eee7b183
BrianMutheeGatitu <brian.gatitu@student.moringaschool.com>
2023-11-05 22:37:25 +0300

    completed assingment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import { directors } from "../data";
 
-function App() {
-  return <div>{/*{code here}*/}</div>;
+function Directors() {
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => (
+        <div key={director.name}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map((movie) => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App;
+export default Directors;
